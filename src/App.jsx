29b2e4d94855d36068fb06610a5747aa15974740
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-02-14 17:20:53 +0530

    hehe

--- src/App.jsx
+++ src/App.jsx
@@ -82,9 +82,9 @@ function App() {
     <div className="block md:hidden">
         <Layout2 /> {/* Mobile Layout (Below 768px) */}
     </div>
-    <div className="hidden md:block lg:hidden">
+       <div className="hidden md:block lg:hidden">
         <Layout1 /> {/* Tablet Layout (768px - 1024px) */}
-      </div>
+       </div>
     <div className="hidden lg:block">
         <Layout1 /> {/* Desktop Layout (Above 1024px) */}
       </div>
